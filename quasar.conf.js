06c6d321b467212ec7262d02079c1a5ad490fd83
Matthew Hotchen <m@mhn.me>
2018-05-22 07:52:17 +0100

    Make sure port value for dev server is actually configured

--- quasar.conf.js
+++ quasar.conf.js
@@ -43,7 +43,7 @@ module.exports = function (ctx) {
     },
     devServer: {
       // https: true,
-      // port: 8080,
+      port: 8080,
       open: true // opens browser window automatically
     },
     // framework: 'all' --- includes everything; for dev only!
